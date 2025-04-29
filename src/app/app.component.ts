410dccb5ef837c326e8df1a409aaf1b7850f0c0c
VishmithaNadeeshan <vishmithanadeeshan20041111@gmail.com>
2025-04-29 11:51:50 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./common/header/header.component";
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'practice-1';
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
+
+export const routes: Routes = [
+    {
+        path:"",
+        component:HeaderComponent
+    }
+];
